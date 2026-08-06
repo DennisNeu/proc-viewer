4d78515a4bbcc3507a5d103b2564d79ea70a074c
Dennis <DennisNeu@users.noreply.github.com>
2026-08-06 20:31:54 +0200

    Refactor

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-
 def main():
     with open("/proc/uptime", "r") as file:
         content = file.read().split()
         # I use content[0] because split turns the string into an array split by whitespace
-        minutes = round(float(content[0]) / 60)
-        seconds = round(float(content[0]) % 60)
-        print(f"The system has been running for {minutes}:{seconds}")
+        time = float(content[0])
+        minutes = round(time / 60)
+        seconds = round(time % 60)
+        print(f"The system has been running for {minutes} minutes and {seconds} seconds")
     
 
 if __name__ == "__main__":
